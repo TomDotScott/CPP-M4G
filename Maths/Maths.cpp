3357613a553719e2bf597560d9b8087f6ca47332
TomDotScott <tom.scott45456@gmail.com>
2021-02-09 11:02:30 +0000

    Vector Class and operations
    Adding, subtracting, magnitude, angles and dotproduct as a templated vector in the maths4games namespace

--- Maths/Maths.cpp
+++ Maths/Maths.cpp
@@ -1,20 +1,87 @@
-// Maths.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+namespace maths4games
+{
+	constexpr double PI{ 3.141592653589793 };
+	
+	template<class T>
+	class Vector2
+	{
+	public:
+		static float DotProduct(const Vector2<T>& x, const Vector2<T>& y)
+		{
+			return x.GetX() * y.GetX() + x.GetY() * y.GetY();
+		}
+
+		static float AngleDeg(Vector2<T>& x, Vector2<T>& y)
+		{
+			return acos(DotProduct(x, y) / (x.Magnitude() * y.Magnitude())) * (180 / PI);
+		}
+
+		static float AngleRad(Vector2<T>& x, Vector2<T>& y)
+		{
+			return acos(DotProduct(x, y) / (x.Magnitude() * y.Magnitude()));
+		}
+
+		Vector2(T x, T y) :
+			m_x(x),
+			m_y(y)
+		{
+		}
+
+		T Magnitude()
+		{
+			return sqrt(pow(m_x, 2) + pow(m_y, 2));
+		}
+
+		T SqrMag()
+		{
+			return pow(m_x, 2) + pow(m_y, 2);
+		}
+
+		Vector2<T> Normalise()
+		{
+			return { m_x / Magnitude(), m_y / Magnitude() };
+		}
+
+
+		void Add(const Vector2<T>& vec)
+		{
+			m_x += vec.m_x;
+			m_y += vec.m_y;
+		}
+
+		void Sub(const Vector2<T>& vec)
+		{
+			m_x = vec.m_x - m_x;
+			m_y = vec.m_y - m_y;
+		}
+
+		T GetX() const { return m_x; }
+		T GetY() const { return m_y; }
+
+	private:
+		T m_x;
+		T m_y;
+	};
+}
+
+
+std::ostream& operator<<(std::ostream& lhs, const maths4games::Vector2<float>& rhs)
+{
+	lhs << rhs.GetX() << " " << rhs.GetY();
+	return lhs;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	maths4games::Vector2<float> v1{ 13, 10 };
+	maths4games::Vector2<float> v2{ 3, 4 };
+
+	std::cout << maths4games::Vector2<float>::AngleDeg(v1, v2) << std::endl;
+	/*v1.Sub(v2);
+	std::cout << v1 << std::endl;
+
+	std::cout << v1.Normalise() << std::endl;*/
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
